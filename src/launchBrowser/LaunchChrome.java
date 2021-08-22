4ab79652a1547b60b6664279456b9e5c85d18315
darshup <88005615+darshup@users.noreply.github.com>
2021-08-22 10:56:50 -0500

    first commit

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,28 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+//		Scanner scn = new Scanner(System.in);
+
+//Remember this. Its code for creating system driver system property. The two line below is the code to launch chrome		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\darsh\\Apr2021_Selenium\\First_Selenium\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.selenium.dev/");
+		driver.manage().window().maximize();
+		
+		Thread.sleep(2000);
+		
+		driver.close();
+		
+		
+	}
+	
+}
